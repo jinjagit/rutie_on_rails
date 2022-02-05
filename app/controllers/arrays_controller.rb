07a212054a66eac3b5214cbe6c50b8e217fe7b5f
Simon Tharby <simon@liquidvoting.io>
2022-02-05 17:12:01 +0000

    ruby def find_blocked_words

--- app/controllers/arrays_controller.rb
+++ app/controllers/arrays_controller.rb
@@ -6,4 +6,114 @@ class ArraysController < ApplicationController
     @text = "Pass array from Ruby to Rust  => #{array.inspect}\n"\
             "Return reversed array to Ruby => #{reversed.inspect}"
   end
+
+  def blocked_words
+    blocked_words =[]
+
+    # Currently 618 words in pl.txt
+    File.readlines(Rails.root.join("config/blocked_words/pl.txt")).each do |line|
+      blocked_words << line.strip
+    end
+
+    words_to_find = get_random_items(blocked_words, 3)
+    text_array = create_array_including_supplied_words(5, words_to_find)
+    found_words = find_blocked_words(text_array, words_to_find)
+
+    @text = "words_to_find: #{words_to_find.inspect}\n"\
+            "text_array: #{text_array.inspect}\n"\
+            "found_words: #{found_words.inspect}\n"\
+            "passed check? #{check_arrays_contain_same_elements(words_to_find, found_words)}"
+  end
+
+  private
+
+  def find_blocked_words(text_array, words_to_find)
+    found_words = []
+
+    text_array.length.times do |i|
+      word = text_array[i]
+      found_words << word if words_to_find.include? word
+      break if found_words.length == words_to_find.length
+    end
+
+    found_words
+  end
+
+  def check_arrays_contain_same_elements(array_a, array_b)
+    if array_a.length != array_b.length
+      raise StandardError.new "Compared arrays are different lengths"
+    end
+
+    passed = true
+
+    array_a.length.times do |i|
+      passed = false unless array_b.include? array_a[i]
+    end
+
+    passed
+  end
+
+  # Includes zero: e.g. n_unique_integers(9, 3)
+  # will return 3 unique integers, randomly chosen from the range 0..8
+  def n_unique_integers (from_n, get_n)
+    check_unique_selection_limit(from_n, get_n)
+
+    integers = []
+      loop do
+        int = rand(from_n)
+        integers << int unless integers.include? int
+        break if integers.length == get_n
+      end
+
+      integers
+  end
+
+  def get_random_items(array, n_items)
+    check_unique_selection_limit(array.length, n_items)
+
+    indices = n_unique_integers(array.length, n_items)
+
+    selected = []
+    n_items.times do |i|
+      selected << array[indices[i]]
+    end
+
+    selected
+  end
+
+  def check_unique_selection_limit(a, b)
+    if b > a
+      raise StandardError.new "Cannot return more unique items than exist"
+    end
+  end
+
+  def replace_items_in_array(array, new_items)
+    check_unique_selection_limit(array.length, new_items.length)
+
+    indices = n_unique_integers(array.length, new_items.length)
+
+    new_items.length.times do |i|
+      array[indices[i]] = new_items[i]
+    end
+
+    array
+  end
+
+  def create_array_of_random_strings(n)
+    array = []
+
+    n.times do
+      num = rand(10) + 1
+      array << (0...num).map { ('a'..'z').to_a[rand(26)] }.join
+    end
+
+    array
+  end
+
+  def create_array_including_supplied_words(n_words, supplied_words)
+    array_of_random_strings = create_array_of_random_strings(n_words)
+    prepared_array = replace_items_in_array(array_of_random_strings, supplied_words)
+
+    prepared_array
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :app_configs
   get '/', to: 'hello#hello'
   get 'arrays/reverse', to: 'arrays#reverse'
+  get 'arrays/blocked_words', to: 'arrays#blocked_words'
 end
