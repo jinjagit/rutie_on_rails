68a60530160d0ebdfc2cac3c99efe608fcc8371f
Simon Tharby <simon@liquidvoting.io>
2022-02-05 12:59:18 +0000

    add hello_world to show can have multiple functions in Rust class

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,7 @@
 class TestsController < ApplicationController
   def index
+    @hello = RustLib.hello_world
+
     array = ['Ruby', 'to', 'Rust']
     reversed = RustLib.reverse(array)
 
--- rust_lib/src/lib.rs
+++ rust_lib/src/lib.rs
@@ -12,6 +12,13 @@ rutie_serde_methods!(
     RustLib,
     _itself,
     ruby_class!(Exception),
+
+    fn pub_hello_world() -> String {
+        let string = format!("Rust says 'Hi!'");
+
+        string
+    }
+
     fn pub_reverse(array: Vec<String>) -> Vec<String> {
         let reverse = array.into_iter().rev().collect();
 
@@ -19,9 +26,11 @@ rutie_serde_methods!(
     }
 );
 
+// Entrypoint function (that Ruby can call):
 #[no_mangle]
 pub extern "C" fn Init_rust_lib() {
     rutie::Class::new("RustLib", None).define(|itself| {
+        itself.def_self("hello_world", pub_hello_world);
         itself.def_self("reverse", pub_reverse);
     });
 }
